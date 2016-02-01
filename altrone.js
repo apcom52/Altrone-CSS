463672121bad4fd3f75aa7afde53f67409ef1c3b
apcom52 <apcom@live.ru>
2016-02-01 23:34:26 +0300

    0.7.0
    + Добавлен Select
    + Добавлены модификаторы bold, italic, underline
    * Исправление ошибок

--- altrone.js
+++ altrone.js
@@ -25,14 +25,46 @@ $(function() {
 				'left': window_width - dropdown_width
 			});
 		}
-		dropdown.slideDown(500);
+		dropdown.slideDown(300);
 	});
 
 	$(document).click(function(e) {
-		if ($(e.target).closest('.dropdown, [data-dropdown-target]').length)
+		if ($(e.target).closest('.dropdown, [data-dropdown-target], .select__menu, .select__options__item').length)
 			return;
-		$('.dropdown').slideUp(500);
+		$('.dropdown').slideUp(300);
+		$('.select__options').slideUp(300);
+		$('.select__menu').removeClass('select__menu--open');
 		e.stopPropagation();
 	});
 
+
+	/* Select Меню */
+	$('body').on('click', '.select__menu', function(event) {
+		var parent = $(this).parent();
+		var select = $(this);
+		var options = parent.find('.select__options');
+		var selected_id = $(this).data('selectValue');
+		console.log(selected_id);
+		console.log(parent);
+		options.find('.select__options__item').removeClass('select__options__item--active');
+		var active_option = options.find('.select__options__item[data-option-id="' + selected_id + '"]');
+		active_option.addClass('select__options__item--active');
+		select.toggleClass('select__menu--open');
+		options.css('width', select.outerWidth());
+		options.css('top', $(this).offset().top + select.outerHeight());
+		options.css('left', $(this).offset().left);
+		options.slideToggle(300);
+
+		$('body').on('click', '.select__options__item', function(e) {
+			var current = $(this);
+			var parentSelect = $(this).parent().parent().find('.select__menu');
+			var currentValue = current.data('optionId');
+			options.find('.select__options__item').removeClass('select__options__item--active');
+			$(this).addClass('select__options__item--active');
+			parentSelect.html(current.html());
+			parentSelect.data('selectValue', currentValue);
+			parentSelect.removeClass('select__menu--open');
+			options.slideUp(300);
+		});
+	});
 });
