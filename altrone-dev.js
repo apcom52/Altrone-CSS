b6e8c8b50d6c452ebd5f60dd3cb1967b5ff69fc3
apcom52 <apcom@live.ru>
2016-03-14 13:43:40 +0300

    0.7.11.0
    + Добавлен блок Feed
    + Добавлен блок User-List
    * Переписан js-компонент sidebar как jquery-плагин

--- altrone-dev.js
+++ altrone-dev.js
@@ -6,6 +6,41 @@ ClassSelector = function(selector) {
 	return document.getElementsByClassName(selector);
 }
 
+ClassList = function(element) {
+	return this;
+}
+
+ClassList.prototype.get = function(element) {
+	var classes = element.className.split(' ');
+	return classes;
+}
+
+ClassList.prototype.class = function(list) {
+	var className = '';
+	list.forEach(function (current, index, list) {
+		className += current + ' ';
+	});
+	className = className.slice(0, -1);
+	return className;
+}
+
+ClassList.prototype.add = function(element, _class) {
+	var list = this.get(element);
+	list.push(_class);
+	element.className = this.class(list);
+}
+
+ClassList.prototype.remove = function(element, _class) {
+	var list = this.get(element);
+	list.forEach(function (current, index, list) {		
+		if (current == _class) {
+			list.splice(index, 1);
+			return;
+		}		
+	});
+	element.className = this.class(list);
+}
+
 function removeClass(element, _class) {
 	var classes = element.className.split(' ');
 	classes.forEach(function (current, index, classes) {		
@@ -21,33 +56,26 @@ function removeClass(element, _class) {
 	});
 	element.className = new_className;
 }
-
-Sidebar = function(id) {
-	this.id = id;
-	this.element = IDSelector(this.id);	
-	this.hidden = true;
-	
-	this.overflow = ClassSelector('overflow');
-
-	return this;
-}
-
-Sidebar.prototype.show = function() {
-	this.hidden = false;
-	this.element.className += ' sidebar--show';
-	this.overflow.className += ' overflow--show';	
-}
-
-Sidebar.prototype.hide = function() {
-	this.hidden = true;
-	removeClass(this.element, 'sidebar--show');
-	removeClass(this.overflow, 'overflow--show');
-}
-
-Sidebar.prototype.toggle = function() {
-	if (this.hidden) {
-		this.show();
-	} else {
-		this.hide();
+/* Sidebar Plugin */
+(function( $ ) {
+	var methods = {
+		show: function() {
+			$(this).addClass('sidebar--show');
+		},
+		hide: function() {
+			$(this).removeClass('sidebar--show');
+		},
+		toggle: function() {
+			$(this).toggleClass('sidebar--show');
+		}
 	}
-}
+  	$.fn.sidebar = function(method) {  		
+  		if (methods[method] ) {
+	        return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+	    } else if ( typeof method === 'object' || ! method ) {
+	        return null;
+	    } else {
+	        $.error( 'Метод "' +  method + '" не найден');
+	    }
+  	};
+})(jQuery);
