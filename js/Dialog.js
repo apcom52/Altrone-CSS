83f8094bba6a46796a5139579a316c8fa830c2e4
DESKTOP-RD8BMT6\apcom <apcom@live.ru>
2017-10-10 22:53:50 +0300

    2.2.0.810
    + Добавлен модификатор .grid--single-line
    + Переработан компонент Feed

--- js/Dialog.js
+++ js/Dialog.js
@@ -1,6 +1,10 @@
 'use strict';
 
 class Dialog {
+    /**
+	 * Constructor of Dialog
+     * @param {Object} props
+     */
 	constructor(props = {}) {
 		let target = this;
 
@@ -15,62 +19,121 @@ class Dialog {
 		target.onHideCallback = props.onHide || null;
 	}
 
+    /**
+	 * Return the title of Dialog
+     * @returns {String}
+     */
 	get title() {
 		return this.__title;
 	}
 
+    /**
+	 * Return the message of Dialog
+     * @returns {String}
+     */
 	get message() {
 		return this.__message;
 	}
 
+    /**
+	 * Return the value of property 'invert'
+     * @returns {bool}
+     */
 	get invert() {
 		return this.__invert;
 	}
 
+    /**
+	 * Return the caption of success button
+     * @returns {String}
+     */
 	get successLabel() {
 		return this.__successLabel;
 	}
 
+    /**
+	 * Return the caption of failed button
+     * @returns {String}
+     */
 	get failLabel() {
 		return this.__failLabel;
 	}
 
+    /**
+	 * Set new title of Dialog
+     * @param {String} value
+     */
 	set title(value) {
 		this.__title = value || 'Empty title';
 	}
 
+    /**
+     * Set new message of Dialog
+     * @param {String} value
+     */
 	set message(value) {
 		this.__message = value || 'Empty message';
 	}
 
+    /**
+     * Set new value of property 'invert'
+     * @param {bool} value
+     */
 	set invert(value) {
 		this.__invert = Boolean(value) || false;
 	}
 
+    /**
+     * Set new caption of success button
+     * @param {String} value
+     */
 	set successLabel(value) {
 		this.__successLabel = value || 'OK';
 	}
 
+    /**
+     * Set new caption of fail button
+     * @param {String} value
+     */
 	set failLabel(value) {
 		this.__failLabel = value || 'Cancel';
 	}
 
+    /**
+     * Set new callback for onSuccessButtonPress Event
+     * @param {Function} func
+     */
 	set onSuccess(func) {
 		this.onSuccessCallback = func || null;
 	}
 
+    /**
+     * Set new callback for onFailButtonPress Event
+     * @param {Function} func
+     */
 	set onFail(func) {
 		this.onFailCallback = func || null;
 	}
 
+    /**
+     * Set new callback for onModalShow Event
+     * @param {Function} func
+     */
 	set onShow(func) {
 		this.onShowCallback = func || null;
 	}
 
+    /**
+     * Set new callback for onModalHide Event
+     * @param {Function} func
+     */
 	set onHide(func) {
 		this.onHideCallback = func || null;
 	}
 
+    /**
+	 * Show dialog
+     */
 	show() {
 		let target = this;
 		
@@ -136,6 +199,9 @@ class Dialog {
 		target.modal.show();
 	}
 
+    /**
+	 * Hide dialog
+     */
 	hide() {
 		let target = this;
 
