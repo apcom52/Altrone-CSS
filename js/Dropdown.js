ffbd6b293e78a9f6e36589c171fe7f3af231d6fc
DESKTOP-RD8BMT6\apcom <apcom@live.ru>
2017-11-05 16:00:08 +0300

    2.2.0.834
    * Теперь выпадающее меню правильно обрабатывает изменение размера окна браузера
    * Убраны лишние и устаревшие компоненты: .user-list, .masonry, .toast-collection

--- js/Dropdown.js
+++ js/Dropdown.js
@@ -1,5 +1,52 @@
 var activeDropdown = null;
 
+function __dropdownSetPosition() {
+    if (activeDropdown) {
+        let position = 'bottom';
+        let target = activeDropdown.objSender;
+        let dropdown = activeDropdown;
+
+        let targetRect = target.getBoundingClientRect();
+
+        if (target.hasAttribute('data-dropdown-position')) {
+            let positionValue = target.getAttribute('data-dropdown-position');
+            if (positionValue == 'left') position = 'left';
+            else if (positionValue == 'right') position = 'right';
+            else if (positionValue == 'top') position = 'top';
+        }
+
+        switch (position) {
+            case 'left':
+                dropdown.style.left = (target.offsetLeft - dropdown.offsetWidth) + 'px';
+                dropdown.style.top = targetRect.offsetTop + 'px';
+                dropdown.classList.add('dropdown--position-left');
+                break;
+            case 'right':
+                dropdown.style.left = (target.offsetLeft + targetRect.width) + 'px';
+                dropdown.style.top = target.offsetTop + 'px';
+                dropdown.classList.add('dropdown--position-right');
+                break;
+            case 'top':
+                dropdown.style.left = target.offsetLeft + 'px';
+                dropdown.style.top = (target.offsetTop - dropdown.offsetHeight) + 'px';
+                dropdown.classList.add('dropdown--position-top');
+                break;
+            case 'bottom':
+            default:
+                dropdown.style.left = target.offsetLeft + 'px';
+                dropdown.style.top = (target.offsetTop + targetRect.height) + 'px';
+                dropdown.classList.add('dropdown--position-bottom');
+                break;
+        }
+
+        console.log(position);
+
+        if ((position == 'top' || position == 'bottom') && targetRect.width >= dropdown.offsetWidth) {
+            dropdown.style.width = targetRect.width + 'px';
+        }
+    }
+}
+
 document.addEventListener('click', function(event) {
     let target = event.target;
     let dropdown = document.getElementById(target.getAttribute('data-dropdown'));
@@ -16,50 +63,17 @@ document.addEventListener('click', function(event) {
         } else {
         	activeDropdown = null;
 		}
+
+        window.removeEventListener('resize', __dropdownSetPosition, false);
     }
 
 	if (target.hasAttribute('data-dropdown')) {
-		let position = 'bottom';
         dropdown.classList.add('dropdown--show');
         activeDropdown = dropdown;
+        activeDropdown.objSender = target;
 
-		let targetRect = target.getBoundingClientRect();
+        __dropdownSetPosition();
 
-		if (target.hasAttribute('data-dropdown-position')) {
-			let positionValue = target.getAttribute('data-dropdown-position');
-			if (positionValue == 'left') position = 'left';
-			else if (positionValue == 'right') position = 'right';
-			else if (positionValue == 'top') position = 'top';
-		}
-
-		switch (position) {
-			case 'left':
-				dropdown.style.left = (target.offsetLeft - dropdown.offsetWidth) + 'px';
-				dropdown.style.top = targetRect.offsetTop + 'px';
-				dropdown.classList.add('dropdown--position-left');
-				break;
-			case 'right':
-                dropdown.style.left = (target.offsetLeft + targetRect.width) + 'px';
-                dropdown.style.top = target.offsetTop + 'px';
-                dropdown.classList.add('dropdown--position-right');
-                break;
-            case 'top':
-                dropdown.style.left = target.offsetLeft + 'px';
-                dropdown.style.top = (target.offsetTop - dropdown.offsetHeight) + 'px';
-                dropdown.classList.add('dropdown--position-top');
-                break;
-			case 'bottom':
-			default:
-                dropdown.style.left = target.offsetLeft + 'px';
-                dropdown.style.top = (target.offsetTop + targetRect.height) + 'px';
-                dropdown.classList.add('dropdown--position-bottom');
-                break;
-		}
-
-		console.log(position);
-
-		if ((position == 'top' || position == 'bottom') && targetRect.width >= dropdown.offsetWidth) {
-			dropdown.style.width = targetRect.width + 'px';
-		}
+        window.addEventListener('resize', __dropdownSetPosition, false);
 	}
 });
