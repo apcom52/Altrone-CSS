ba83e950fd82f79591c7e7e636dd5dd9ac11d906
DESKTOP-RD8BMT6\apcom <apcom@live.ru>
2017-11-12 02:41:19 +0300

    3.0.0.841
    + Добавлен новый класс-утилита для удобной работы с куки-файлами

--- js/Utils.js
+++ js/Utils.js
@@ -13,3 +13,49 @@ Array.prototype.random = function() {
     let items = this.slice();
     return items[Math.floor(Math.random()*items.length)];
 };
+
+class Cookies {
+    isEnabled() {
+        return navigator.cookieEnabled;
+    }
+
+    getCookie(name) {
+        let matches = document.cookie.match(new RegExp(
+            "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+        ));
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
+
+    setCookie(name, value, options = {}) {
+        let expires = options.expires;
+        if (typeof expires == "number" && expires) {
+            let d = new Date();
+            d.setTime(d.getTime() + expires * 1000);
+            expires = options.expires = d;
+        }
+
+        if (expires && expires.toUTCString) {
+            options.expires = expires.toUTCString();
+        }
+
+        value = encodeURIComponent(value);
+
+        let updatedCookie = name + "=" + value;
+
+        for (let propName in options) {
+            updatedCookie += "; " + propName;
+            let propValue = options[propName];
+            if (propValue !== true) {
+                updatedCookie += "=" + propValue;
+            }
+        }
+
+        document.cookie = updatedCookie;
+    }
+
+    deleteCookie(name) {
+        this.setCookie(name, "", {
+            expires: -1
+        });
+    }
+}
