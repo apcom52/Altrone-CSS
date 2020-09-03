7a9500b271f7078480e37acb75ab0325dd8f89fd
apcom <apcom@live.ru>
2020-09-03 23:49:31 +0300

    - Начата работа над компонентом Input-Group - Написан компонент Form-Control - Написан компонент Form-Group - Написан компонент Input - Изменено значение переменной smallFont

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
 const browserSync = require('browser-sync').create();
 
 function css() {
+	const onError = (err) => {
+		notify.onError({
+			title: "Gulp",
+			subtitle: "build failed",
+			message: "Error: <%= error.message %>",
+			sound: "Pop"
+		})(err);
+
+		this.emit('end');
+	}
+
 	return gulp.src('src/scss/altrone.scss')
+		.pipe(plumber({ errorHandler: onError }))
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('dist'))
 		.pipe(browserSync.stream());
@@ -21,7 +35,8 @@ function watch() {
 			baseDir: './dist',
 			index: '/index.html',
 			cors: true
-		}
+		},
+		tunnel: true
 	});
 	gulp.watch('src/scss/**/*.scss', css);
 	gulp.watch('src/index.html', html);
